f6231b256a402a92ccf57ce2b30b569bf4d43e06
Giannis Tzonis <tzonis.john@hotmail.com>
2022-11-02 06:06:09 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
+import Joke from "./Joke";
+import jokesData from "./jokesData";
 
-export default function Joke(props) {
-  const [isShown, setIsShown] = React.useState(false);
-  function toggleShown() {
-    setIsShown((prevShown) => !prevShown);
-  }
-  return (
-    <div>
-      {props.setup && <h3>{props.setup}</h3>}
-      {isShown && <p>{props.punchline}</p>}
-      <button onClick={toggleShown}>
-        {isShown ? "Hide" : "Show"} Punchline
-      </button>
-      <hr />
-    </div>
-  );
+export default function App() {
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return <div>{jokeElements}</div>;
 }
